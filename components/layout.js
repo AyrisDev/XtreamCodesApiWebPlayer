80c91e5373e19782318b025590d90235f34d6931
AyrisDev <ayris.dev@gmail.com>
2023-03-07 23:03:20 +0300

    b

--- components/layout.js
+++ components/layout.js
@@ -1,19 +1,7 @@
 import React from "react";
 import Footer from "./footer";
 import Navigation from "./navigation";
-
-export default function layout({
-  children,
-  movie,
-  live,
-  seriesCate,
-}: {
-  lastVod: Array<string | number | any>,
-
-  movie: Array<string | number | any>,
-  live: Array<string | number | any>,
-  seriesCate: Array<string | number | any>,
-}) {
+const layout = ({ children, movie, live, seriesCate }) => {
   return (
     <div className=" text-app-pure-white lg:flex   ">
       <main className="mx-0 flex flex-col py-6 px-4 md:m-6 md:px-0 md:pt-0 lg:ml-32 lg:min-w-[1024px]  lg:grow ">
@@ -23,4 +11,6 @@ export default function layout({
       </main>
     </div>
   );
-}
+};
+
+export default layout;
