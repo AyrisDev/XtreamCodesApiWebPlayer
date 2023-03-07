309e0d6ba0576a24a6365f16154dc24657789cf3
AyrisDev <ayris.dev@gmail.com>
2023-03-07 07:02:01 +0300

    b

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -6,13 +6,14 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/free-mode";
 import { Autoplay, Pagination, Navigation, FreeMode } from "swiper";
-const Slider = ({
+
+export default function Slider({
   lastVod,
   lastTime,
 }: {
   lastVod: Array<string | number | any>;
   lastTime: number;
-}) => {
+}) {
   return (
     <div className="flex space-x-6 space-y-6 overflow-y-hidden overflow-x-scroll my-10 ml-36 mr-10 scrollbar justify-center items-center drop-shadow-xl  scrollbar-hide scrollbar scrollbar-thin scrollbar-thumb-[#10121b]  scrollbar-track-[#10121b]/30 pb-4  ">
       <Swiper
@@ -49,10 +50,10 @@ const Slider = ({
                       <h1 className="text-yellow-800 z-50  font-bold font-oswald">
                         {filterLastVod.name}
                       </h1>
-                      <span class="absolute right-8 bottom-2 justify-end  text-yellow-800 font-bold rounded-lg text-xs  mr-2 px-2.5 py-0.5 ">
+                      <span className="absolute right-8 bottom-2 justify-end  text-yellow-800 font-bold rounded-lg text-xs  mr-2 px-2.5 py-0.5 ">
                         Rating
                       </span>
-                      <span class="absolute right-0 bottom-2 justify-end bg-purple-900 text-white rounded-lg text-xs font-medium mr-2 px-2.5 py-0.5">
+                      <span className="absolute right-0 bottom-2 justify-end bg-purple-900 text-white rounded-lg text-xs font-medium mr-2 px-2.5 py-0.5">
                         {filterLastVod.rating}
                       </span>
                     </div>
@@ -71,6 +72,4 @@ const Slider = ({
       </Swiper>
     </div>
   );
-};
-
-export default Slider;
+}
