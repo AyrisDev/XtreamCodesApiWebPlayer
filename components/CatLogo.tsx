1a8d0291acd440f270d6afd9b76cb6b34306231e
AyrisDev <ayris.dev@gmail.com>
2023-03-07 05:43:59 +0300

    b

--- components/CatLogo.tsx
+++ components/CatLogo.tsx
@@ -5,7 +5,7 @@ const CatLogo = () => {
   return (
     <section className="flex flex-col md:flex-row justify-center items-center mt-10 gap-6 px-8 max-w-[1400px] mx-auto">
       <div className="flex relative w-52 h-32 sm:w-64 sm:h-36 border-[3px] border-[#f9f9f9] border-opacity-10 rounded-lg cursor-pointer shadow-xl overflow-hidden  hover:border-opacity-80 hover:shadow-2xl transform hover:scale-105 transition duration-300 group">
-        <img src="/images/disnep.png" layout="fill" objectFit="cover" />
+        <img src="/images/disnep.png" className="object-cover object-fill" />
         <video
           autoPlay
           loop
@@ -15,7 +15,7 @@ const CatLogo = () => {
         </video>
       </div>
       <div className="flex relative w-52 h-32 sm:w-64 sm:h-36 border-[3px] border-[#f9f9f9] border-opacity-10 rounded-lg cursor-pointer shadow-xl overflow-hidden  hover:border-opacity-80 hover:shadow-2xl transform hover:scale-105 transition duration-300 group">
-        <img src="/images/pixar.png" layout="fill" objectFit="cover" />
+        <img src="/images/pixar.png" className="object-cover object-fill" />
         <video
           autoPlay
           loop
@@ -26,7 +26,7 @@ const CatLogo = () => {
       </div>
 
       <div className="flex relative w-52 h-32 sm:w-64 sm:h-36 border-[3px] border-[#f9f9f9] border-opacity-10 rounded-lg cursor-pointer shadow-xl overflow-hidden  hover:border-opacity-80 hover:shadow-2xl transform hover:scale-105 transition duration-300 group">
-        <img src="/images/marvel.png" layout="fill" objectFit="cover" />
+        <img src="/images/marvel.png" className="object-cover object-fill" />
         <video
           autoPlay
           loop
@@ -37,7 +37,7 @@ const CatLogo = () => {
       </div>
 
       <div className="flex relative w-52 h-32 sm:w-64 sm:h-36 border-[3px] border-[#f9f9f9] border-opacity-10 rounded-lg cursor-pointer shadow-xl overflow-hidden  hover:border-opacity-80 hover:shadow-2xl transform hover:scale-105 transition duration-300 group">
-        <img src="/images/starwars.png" layout="fill" objectFit="cover" />
+        <img src="/images/starwars.png" className="object-cover object-fill" />
         <video
           autoPlay
           loop
@@ -50,8 +50,7 @@ const CatLogo = () => {
       <div className="flex relative w-52 h-32 sm:w-64 sm:h-36 border-[3px] border-[#f9f9f9] border-opacity-10 rounded-lg cursor-pointer shadow-xl overflow-hidden  hover:border-opacity-80 hover:shadow-2xl transform hover:scale-105 transition duration-300 group">
         <img
           src="/images/national-geographic.png"
-          layout="fill"
-          objectFit="cover"
+          className="object-cover object-fill"
         />
         <video
           autoPlay
