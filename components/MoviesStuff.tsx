43e43285d2852dbdc170972b71da2cba121f497f
AyrisDev <ayris.dev@gmail.com>
2023-03-07 06:09:41 +0300

    b

--- components/MoviesStuff.tsx
+++ components/MoviesStuff.tsx
@@ -6,7 +6,7 @@ import "swiper/css/pagination";
 import "swiper/css/free-mode";
 import { Autoplay, Pagination, Navigation, FreeMode } from "swiper";
 
-const MoviesStuff = ({ results, title, lastTime }) => {
+const MoviesStuff = ({ Lseries, title, lastTime }) => {
   return (
     <div className="relative flex flex-col space-y-2 my-10 px-8 max-w-[1400px] mx-auto">
       <h2 className="font-semibold">{title}</h2>
@@ -21,24 +21,24 @@ const MoviesStuff = ({ results, title, lastTime }) => {
           spaceBetween={20}
           modules={[FreeMode, Pagination, Autoplay]}
           className=" w-full rounded rounded-xl scrollbar-thin scrollbar-thumb-[#10121b]/30  scrollbar-track-transparent overflow-hidden overflow-x-hidden">
-          {results ? (
+          {Lseries ? (
             <>
-              {results
-                .filter((lastVoda) => lastVoda.last_modified > lastTime)
-                .map((filterLastVod) => (
-                  <SwiperSlide
-                    key={filterLastVod.stream_id}
-                    className="flex rounded-lg w-[330px] h-[210px] overflow-hidden shadow-xl cursor-pointer border-[3px] border-[#f9f9f9] border-opacity-10  hover:border-opacity-80 hover:shadow-2xl transform hover:scale-105 transition duration-300 ">
-                    <a
-                      href={`/${filterLastVod.category_id}/${filterLastVod.stream_id} `}
-                      className="cursor-pointer w-[330px] h-[210px]">
-                      <img
-                        src={filterLastVod.cover}
-                        className="object-fill w-[330px] h-[400px] rounded-lg"
-                      />
-                    </a>
-                  </SwiperSlide>
-                ))}
+              {Lseries.filter(
+                (lastVoda) => lastVoda.last_modified > lastTime
+              ).map((filterLastVod) => (
+                <SwiperSlide
+                  key={filterLastVod.stream_id}
+                  className="flex rounded-lg w-[330px] h-[210px] overflow-hidden shadow-xl cursor-pointer border-[3px] border-[#f9f9f9] border-opacity-10  hover:border-opacity-80 hover:shadow-2xl transform hover:scale-105 transition duration-300 ">
+                  <a
+                    href={`/${filterLastVod.category_id}/${filterLastVod.stream_id} `}
+                    className="cursor-pointer w-[330px] h-[210px]">
+                    <img
+                      src={filterLastVod.cover}
+                      className="object-fill w-[330px] h-[400px] rounded-lg"
+                    />
+                  </a>
+                </SwiperSlide>
+              ))}
             </>
           ) : (
             <>
