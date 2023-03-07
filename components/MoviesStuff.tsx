969b774e2a7dd2b622ba323b834558b0d4c4c68e
AyrisDev <ayris.dev@gmail.com>
2023-03-07 06:30:11 +0300

    b

--- components/MoviesStuff.tsx
+++ components/MoviesStuff.tsx
@@ -6,7 +6,7 @@ import "swiper/css/pagination";
 import "swiper/css/free-mode";
 import { Autoplay, Pagination, Navigation, FreeMode } from "swiper";
 
-export default function MoviesStuff = ({ lastSeries, title, lastTime }) => {
+export default function MoviesStuff({ lastSeries, title, lastTime }) {
   return (
     <div className="relative flex flex-col space-y-2 my-10 px-8 max-w-[1400px] mx-auto">
       <h2 className="font-semibold">{title}</h2>
@@ -51,7 +51,5 @@ export default function MoviesStuff = ({ lastSeries, title, lastTime }) => {
         </Swiper>
       </div>
     </div>
-
-    );
-  }
- 
+  );
+}
